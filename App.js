c528943222d9610a9a5b5cc274f75fbc33fd1463
RianLimeira <rianlimeira@gmail.com>
2023-08-21 21:44:37 -0300

    commit css tailwild

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Dimensions, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+// import {styles} from './src/theme'
+import {Container, Title, SubTitle} from './src/theme/style'
+import { styled } from 'nativewind';
+
+const ViewStyled = styled(View);
 
 export default function App() {
+  // console.log(Platform.OS);
+//   Alert.alert('Alerta personalizado','Minha notificação',
+//   [{
+//     text: 'Valeu',
+//     onPress: () => Alert.alert('Alerta 2', 'Valeu')
+//   },
+//   {
+//     text:'Boa',
+//     onPress:() => Alert.alert('Ok', 'Juntos')
+//   }
+// ])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <View style={styles.container}>
+    <Container>
+      <Title>Hello World! Your first project App</Title>
+      <TextInput placeholder='Seu Nome' placeholderTextColor={'red'}></TextInput>
+      <TouchableOpacity activeOpacity={.5}>
+        <Text>Botão personalizado</Text>
+      </TouchableOpacity>
+      {/* <Text style={{ fontSize: 20, color: '#fff'}}> {Dimensions.get('window').scale}</Text> */}
+      <SubTitle>{Dimensions.get('window').scale}</SubTitle>
       <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      </Container>
+    // </View>
+  )
+};
---
+++ src/theme/style/index.js
@@ -0,0 +1,20 @@
+import { styled } from "styled-components/native";
+
+export const Container = styled.View`
+    background-color: #000;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+export const Title = styled.Text`
+    color: orange;
+    font-size: 16px;
+`;
+
+export const SubTitle = styled.Text`
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
+`;
