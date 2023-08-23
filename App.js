e7caa0a94eb23dbe96867c9bfcb0aa36be90ef93
RianLimeira <rianlimeira@gmail.com>
2023-08-22 22:06:33 -0300

    correção tailwind css

--- App.js
+++ App.js
@@ -1,35 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { Alert, Dimensions, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-// import {styles} from './src/theme'
+import {styles} from './src/theme'
 import {Container, Title, SubTitle} from './src/theme/style'
 import { styled } from 'nativewind';
+
 
 const ViewStyled = styled(View);
 
 export default function App() {
-  // console.log(Platform.OS);
-//   Alert.alert('Alerta personalizado','Minha notificação',
-//   [{
-//     text: 'Valeu',
-//     onPress: () => Alert.alert('Alerta 2', 'Valeu')
-//   },
-//   {
-//     text:'Boa',
-//     onPress:() => Alert.alert('Ok', 'Juntos')
-//   }
-// ])
+
   return (
-    // <View style={styles.container}>
-    <Container>
-      <Title>Hello World! Your first project App</Title>
-      <TextInput placeholder='Seu Nome' placeholderTextColor={'red'}></TextInput>
-      <TouchableOpacity activeOpacity={.5}>
-        <Text>Botão personalizado</Text>
-      </TouchableOpacity>
-      {/* <Text style={{ fontSize: 20, color: '#fff'}}> {Dimensions.get('window').scale}</Text> */}
-      <SubTitle>{Dimensions.get('window').scale}</SubTitle>
-      <StatusBar style="auto" />
-      </Container>
-    // </View>
-  )
-};
+    <ViewStyled className='flex-1 items-center justify-center bg-black'>
+      <StatusBar barStyle={'light-content'} backgroundColor='red' />
+      <Text className='text-xs text-red-900'>Hello World!</Text>
+      <Text className='text-base text-orange-400 font-bold'>My first project App</Text>
+
+    </ViewStyled>
+  );
+}
+
