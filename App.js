9efa0ffc1fc4ea65b0d5eb66d58d75ad456f85af
RianLimeira <rianlimeira@gmail.com>
2023-08-30 22:38:42 -0300

    Tela TopTabs-Whats

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import StackRouter from "./src/routes/stack";
 import DrawerRouter from "./src/routes/drawer";
+import TopTabsRouter from "./src/routes/topTabs";
 
 export default function App() {
 
   return (
     <NavigationContainer>
       {/* <StackRouter /> */}
-      <DrawerRouter />
+      {/* <DrawerRouter /> */}
+      <TopTabsRouter />
     </NavigationContainer>
   );
 }
---
+++ src/routes/topTabs/index.js
@@ -0,0 +1,24 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"; 
+import Home from "../../screens/Home";
+import Details from "../../screens/Details";
+
+const TopTabs = createMaterialTopTabNavigator();
+
+export default function TopTabsRouter(){
+    return(
+        <TopTabs.Navigator className='top-8'>
+            <TopTabs.Screen options={{
+                headerTitle: 'Tela Inicial',
+                headerShown: true,
+                headerStyle: {
+                    backgroundColor: 'red',
+                    paddingTop: 130,
+                    
+                  },
+            }} name="Home" component={Home} />
+            <TopTabs.Screen options={{
+                headerTitle: 'Detalhes Produtos'
+            }} name="Details" component={Details} />
+        </TopTabs.Navigator>
+    )
+}
