4ba95aa2727fd0c68d77e65d6a2577296e573942
RianLimeira <rianlimeira@gmail.com>
2023-09-27 14:18:52 -0300

    animação com react animatable

--- src/screens/Animation/index.js
+++ src/screens/Animation/index.js
@@ -1,28 +1,46 @@
-import { Button, Text, View } from "react-native";
-import Animated, {useSharedValue, withTiming, Easing, useAnimatedStyle } from "react-native-reanimated";
+import { Button, Text, View, Image } from "react-native";
+// import Animated, {useSharedValue, withTiming, Easing, useAnimatedStyle } from "react-native-reanimated";
+import * as Animated from 'react-native-animatable'
 
+//criar propria animação com imagens
+// const ImageAnimated = Animated.createAnimatableComponent(Image)
+
+const ButtonAnimated = Animated.createAnimatableComponent(Button);
+
+const fadeIn = {
+    from:{
+        opacity: 0,
+    },
+    to:{
+        opacity:1,
+    },
+};
 
 export default function Animation(){
-    const randomWidht = useSharedValue(10);
+    // const randomWidht = useSharedValue(10);
 
-    const style = useAnimatedStyle(() => {
-        return{
-            width: withTiming(randomWidht.value, {
-                easing: Easing.ease,
-                duration: 500
-            })
-        }
-    })
+    // const style = useAnimatedStyle(() => {
+    //     return{
+    //         width: withTiming(randomWidht.value, {
+    //             easing: Easing.ease,
+    //             duration: 500
+    //         })
+    //     }
+    // })
+
+
 
     return(
         <View className='flex-1 items-center mt-10' >
-            <Text className='font-bold text-lg text-center justify-center'>Trabalhar com Animações com Reanimation</Text>
-            <Animated.View style={[{
-                width:100, height:80, backgroundColor: '#000', margin: 30
-            }, style]} />
-                <Button title="toggle" onPress={() => {
-                    randomWidht.value = Math.random() * 500;
-                }} />
+            <Animated.Text animation={'slideInDown'} iterationCount={'infinite'} direction={'alternate'} className='font-bold text-lg text-center justify-center'>Trabalhar com Animações com Animated</Animated.Text>
+            
+                {/* <Button title="toggle" /> */}
+
+                <Animated.Text animation={fadeIn} easing={'ease-in-out'} iterationCount={'infinite'}>
+                    Hello
+                </Animated.Text>
+
+                {/* <ImageAnimated animation={''} /> */}
             </View>
         
     )
