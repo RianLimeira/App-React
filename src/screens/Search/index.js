d87f8b0d27d69e197f40124122f0ddb4f2778364
RianLimeira <rianlimeira@gmail.com>
2023-11-03 18:59:47 -0300

    create screen search pokemon

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -2,6 +2,11 @@ import { Text, TextInput, TouchableOpacity } from "react-native";
 import { View } from "react-native-animatable";
 
 export default function Search() {
+
+    async function searchPokemon(){
+        
+    }
+
   return (
     <View className="flex-1 space-x-2 bg-slate-300">
       <TextInput
@@ -12,7 +17,7 @@ export default function Search() {
       />
       <View className="flex-row space-x-4 items-center self-center mt-6 rounded">
         <TouchableOpacity
-          onPress={handleCreateUser}
+          onPress={searchPokemon}
           className="w-20 h-[40px] bg-red-600 items-center justify-center"
         >
           <Text className="items-center justify-center self-center">
